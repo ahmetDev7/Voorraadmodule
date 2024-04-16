04acb9504c1b1673b20a66c391d90dfbff3c1eeb
ahmetmuratdagi7 <ahmetmuratdagi7@hotmail.com>
2024-04-16 13:04:54 +0200

    updates

---
+++ voorraadmodule/resources/views/producten.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="producten-pagina">
+    <h1>PRODUCTEN PAGINA</h1>
+    <a class="add-button">
+        <p>Product toevoegen</p>
+    </a>
+</div>
+@stop

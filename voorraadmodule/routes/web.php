a89b5070704e7bee162be1dff2c31753e32eb98b
NotATree1 <144020936+NotATree1@users.noreply.github.com>
2024-04-18 12:58:58 +0200

    Revert "Updateproduct"

--- voorraadmodule/routes/web.php
+++ voorraadmodule/routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/producten/', [ProductController::class, 'index'])->name('products.index');
+Route::get('/producten/', [ProductController::class,'index'])->name('products.index');
 Route::get('/producten/toevoegen', [App\Http\Controllers\AddProductController::class, 'index'])->name('products.index');
 Route::post('/producten/toevoegen', [App\Http\Controllers\AddProductController::class, 'add'])->name('products.add');
-Route::get('/producten/{id}/aanpassen', [App\Http\Controllers\UpdateProductController::class, 'edit'])->name('products.edit');
-Route::put('/producten/{id}/aanpassen', [App\Http\Controllers\UpdateProductController::class, 'update'])->name('products.update');

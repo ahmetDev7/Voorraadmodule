cb081f337d705b98ad6938d7cc8d3cbdd693025f
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-05-30 13:01:42 +0200

    API Functionality fixed for product and werknemer

--- voorraadmodule/routes/api.php
+++ voorraadmodule/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/product/scan', function (Request $request) {
+    $productId = $request->input('product_id');
+    $werknemerId = $request->input('werknemer_id');
+
+    return response()->json([
+        'message' => 'Product scanned',
+        'product_id' => $productId,
+        'werknemer_id' => $werknemerId
+    ]);
+});

5d33a7d4aac296bc30b88ea1ca90c9676c480349
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-06-05 15:19:10 +0200

    Removed serialnumber from add new product page (form)

--- voorraadmodule/app/Http/Controllers/AddProductController.php
+++ voorraadmodule/app/Http/Controllers/AddProductController.php
@@ -38,45 +38,7 @@ class AddProductController extends Controller
 
         $product->save();
 
-        if ($request->filled('existing_product_id')) {
-            // Add serial number to existing product
-            $product = Product::find($request->input('existing_product_id'));
-
-            $request->validate([
-                'serialnumber_exist' => 'required',
-            ]);
-
-            $productSerialNumber = new ProductSerialNumber();
-            $productSerialNumber->product_id = $product->id;
-            $productSerialNumber->serialnumber = $request->input('serialnumber_exist');
-            $productSerialNumber->productnumber = $product->productnummer;
-            $productSerialNumber->save();
-
-            return redirect()->back()->with('success', 'Serial number toegevoegd aan het bestaande product!');
-        } else {
-            $request->validate([
-                'productnummer' => 'required|unique:products,productnummer',
-                'serialnumber' => 'required',
-                'name' => 'required',
-                'description' => 'required|string|max:255',
-                'category' => 'required'
-            ]);
-
-            $product = new Product();
-            $product->productnummer = $request->input('productnummer');
-            $product->name = $request->input('name');
-            $product->description = $request->input('description');
-            $product->category = $request->input('category');
-            $product->save();
-
-            $productSerialNumber = new ProductSerialNumber();
-            $productSerialNumber->product_id = $product->id;
-            $productSerialNumber->serialnumber = $request->input('serialnumber');
-            $productSerialNumber->productnumber = $product->productnummer;
-            $productSerialNumber->save();
-
-            return redirect()->back()->with('success', 'Nieuw product en serienummer toegevoegd aan de database!');
-        }
+        return redirect()->back()->with('success', 'Nieuw product toegevoegd aan de database!');
     }
 
 

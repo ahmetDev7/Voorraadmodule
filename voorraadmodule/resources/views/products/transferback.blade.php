3e23b1c0d160a73870181b26af566144f9ccf7cd
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-06-13 14:30:57 +0200

    Product Leaves employee

--- voorraadmodule/resources/views/products/transferback.blade.php
+++ voorraadmodule/resources/views/products/transferback.blade.php
@@ -1,33 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
-<form action="{{ route('product.done') }}" class="default-form" method="POST">
+<form action="{{ route('product.done') }}" class="default-form" method="POST" style="color: black;">
     @csrf
-    <input type="hidden" name="ownerid" value="{{ $owner->id}}">
+    <input type="hidden" name="ownerid" value="{{ $owner->id }}">
+    <input type="hidden" name="productid" value="{{ $product->id }}">
     <input type="hidden" name="productid" value="{{ $product->id }}">
 
-
-    <label for="quantity">Hoeveelheid beschikbaar: {{$quantity}}</label>
-    <input type="number" name="quantity" id="quantity" min="1" max="{{ $quantity }}">
-
-    <label for="warehouse">Selecteer een magazijn</label>
-    <select name="warehouse" id="warehouse">
+    <label for="warehouse" style="color: black;">Selecteer een magazijn</label>
+    <select name="warehouse" id="warehouse" style="color: black;">
         @foreach($warehouses as $warehouse)
             <option value="{{ $warehouse->id }}">{{ $warehouse->name }}</option>
         @endforeach
     </select>
 
-
-
-    <button type="submit">verzenden</button>
+    <button type="submit" style="color: black;">verzenden</button>
 </form>
-
-<script>
-    document.getElementById('quantity').addEventListener('input', function () {
-        var maxQuantity = {{ $quantity }};
-        if (parseInt(this.value) > maxQuantity) {
-            this.value = maxQuantity;
-        }
-    });
-</script>
 @stop
